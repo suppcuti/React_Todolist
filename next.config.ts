598ca9f08da8d96b8496a21ac28c28405b92fc66
suppcuti <khongdotot@gmail.com>
2025-03-03 16:50:24 +0700

    DASDASDASDAS

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Enable static export
-  basePath: "/React_Todolist", // Replace with your repo name
-  assetPrefix: "/React_Todolist/", // Ensure assets load correctly
-  images: {
-    unoptimized: true, // Disable Next.js image optimization (GitHub Pages doesn't support it)
-  },
+  /* config options here */
 };
 
 export default nextConfig;
